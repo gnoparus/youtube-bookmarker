2d3bcb77397ca17daa2f3a773f88881ce3a661d6
gnoparus <surapong@gmail.com>
2023-01-15 01:20:58 +0700

    init import

--- popup.js
+++ popup.js
@@ -1,12 +1,29 @@
-// adding a new bookmark row to the popup
+import { getActiveTabURL } from "./utils";
+
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
 
-const onPlay = e => {};
+const onPlay = () => {};
 
-const onDelete = e => {};
+const onDelete = () => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveTabURL();
+  const queryParameters = activeTab.url.split("?")[1];
+  const urlParameters = new URLSearchParams(queryParameters);
+
+  const currentVideo = urlParameters.get("v");
+
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (data) => {
+      const currentVideoBookmarks = data[currentVideo]
+        ? JSON.parse(data[currentVideo])
+        : [];
+    });
+
+    // viewBookmarks
+  }
+});
